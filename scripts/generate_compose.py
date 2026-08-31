bc5675a490ebc6655f4ffa1b96a7466c1a82dea3
JuampiAmboage <juampiamboage18@gmail.com>
2026-08-31 18:13:32 -0300

    feat: update client configuration to include input and output file paths

--- scripts/generate_compose.py
+++ scripts/generate_compose.py
@@ -22,10 +22,15 @@ CLIENT_TEMPLATE = """
     container_name: client_{agency_id}
     depends_on:
       - server
+    volumes:
+      - ./input:/input:ro
+      - ./output:/output
     environment:
       - AGENCY_ID={agency_id}
       - SERVER_HOST=server
       - SERVER_PORT=5678
+      - INPUT_FILE=/input/input-{agency_id}.csv
+      - OUTPUT_FILE=/output/output-{agency_id}.csv
 """
 
 OUTPUT_PATH = "docker-compose.yaml"
